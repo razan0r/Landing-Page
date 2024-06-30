694c41f55c70069c9d8d88a04c3c373e284ee19f
razan0r <razanhamad815@gmail.com>
2024-07-01 00:11:08 +0300

    edit

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,5 +1,4 @@
-
-// When you add a sketch, it automatically adds it to Navbar
+// When you add a section, it automatically adds it to Navbar
 document.addEventListener('DOMContentLoaded', () => {
     const navList = document.getElementById('nav-list');
     const sections = document.querySelectorAll('section');
@@ -13,10 +12,44 @@ document.addEventListener('DOMContentLoaded', () => {
         li.appendChild(a);
         navList.appendChild(li);
     });
+
+    const navLinks = document.querySelectorAll('.nav-link');
+    
+    const options = {
+        root: null,
+        threshold: 0.7
+    };
+
+    // Observes sections to add/remove active class based on viewport visibility
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                navLinks.forEach(link => {
+                    link.classList.remove('active');
+                    if (link.getAttribute('href').substring(1) === entry.target.id) {
+                        link.classList.add('active');
+                    }
+                });
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+
+    // Smooth scrolling to target section when a navigation link is clicked
+    navLinks.forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
 });
 
-//Add an event listener for the scroll event on the document
-
+// Add an event listener for the scroll event on the document
 document.addEventListener('scroll', () => {
     const sections = document.querySelectorAll('section');
     const navLinks = document.querySelectorAll('.nav-link');
@@ -33,18 +66,7 @@ document.addEventListener('scroll', () => {
     });
 });
 
-
-//enables smooth scrolling to a target section when a navigation link is clicked,
-document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('click', event => {
-        event.preventDefault();
-        const targetId = link.getAttribute('href').substring(1);
-        const targetSection = document.getElementById(targetId);
-        targetSection.scrollIntoView({ behavior: 'smooth' });
-    });
-});
-
-// shows the navbar while scrolling and hides it 2 seconds after the scrolling stops.
+// Shows the navbar while scrolling and hides it 2 seconds after the scrolling stops
 let isScrolling;
 document.addEventListener('scroll', () => {
     document.getElementById('navbar').style.display = 'block';
@@ -54,7 +76,7 @@ document.addEventListener('scroll', () => {
     }, 2000);
 });
 
-// displays a "scroll to top" button 
+// Displays a "scroll to top" button
 const scrollToTopBtn = document.getElementById('scroll-to-top');
 window.addEventListener('scroll', () => {
     if (window.scrollY > 100) {
@@ -63,7 +85,8 @@ window.addEventListener('scroll', () => {
         scrollToTopBtn.style.display = 'none';
     }
 });
-//enables smooth scrolling to the top of the page when the "scroll to top" button is clicked.
+
+// Enables smooth scrolling to the top of the page when the "scroll to top" button is clicked
 scrollToTopBtn.addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
